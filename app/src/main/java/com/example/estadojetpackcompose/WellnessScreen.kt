28c632f881907f0197880d3a9bbd7cf0841c6b2b
Mario Nambo <s22120195@alumnos.itsur.edu.mx>
2026-09-22 18:52:03 -0600

    Se dividio water counter en 2 partes, una para la interfaz y otra para llamar al contadpr

--- app/src/main/java/com/example/estadojetpackcompose/WellnessScreen.kt
+++ app/src/main/java/com/example/estadojetpackcompose/WellnessScreen.kt
@@ -3,10 +3,15 @@ package com.example.estadojetpackcompose
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun WellnessScreen(modifier: Modifier= Modifier){
-  WaterCounter(modifier)
+
+fun WellnessScreen(modifier: Modifier = Modifier) {
+  StatefulCounter(modifier)
 }
